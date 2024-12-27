e479294fafe03325b6a79a7be34da0a77a94c4e0
premkumar-004 <premkumarkardale@gmail.com>
2024-12-27 13:17:21 +0530

    creating a basic to-do list using react

---
+++ To-Do/src/ToDoList.jsx
@@ -0,0 +1,12 @@
+export default function ToDoList() {
+    return (
+        <div>
+            <h1>My To-Do List</h1>
+            <ul>
+                <li>Learn React</li>
+                <li>Learn Vite</li>
+                <li>Build something awesome</li>
+            </ul>
+        </div>
+    )
+}
