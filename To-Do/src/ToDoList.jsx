d806098ca7167862b7d33fc33dbc35066c0d2605
premkumar-004 <premkumarkardale@gmail.com>
2024-12-28 16:22:26 +0530

    final commit

--- To-Do/src/ToDoList.jsx
+++ To-Do/src/ToDoList.jsx
@@ -1,12 +1,52 @@
+import React, { useState } from "react";
+
 export default function ToDoList() {
+    let inputStyling = {
+        width: "300px",
+        height: "3rem",
+        textAlign: "center",
+        margin: "1rem",
+    };
+    let btnStyling = {
+        marginBottom: "2rem",
+        marginTop: "1rem",
+    };
+
+    let [todos, setTodos] = useState(["Sample Task"]);
+    let [newTodo, setNewTodo] = useState("");
+
+    let addNewTask = () => {
+        if (newTodo.trim() !== "") {
+            setTodos([...todos, newTodo]);
+            setNewTodo("");
+        } else {
+            alert("Task cannot be empty!");
+        }
+    };
+
+    let updateTodoValue = (event) => {
+        setNewTodo(event.target.value);
+    };
+
     return (
         <div>
-            <h1>My To-Do List</h1>
+            <input
+                type="text"
+                placeholder="Add a task"
+                style={inputStyling}
+                value={newTodo}
+                onChange={updateTodoValue}
+            /><br />
+            <button style={btnStyling} onClick={addNewTask}>
+                Add Task
+            </button>
+            <hr />
+            <h4>To Do List</h4>
             <ul>
-                <li>Learn React</li>
-                <li>Learn Vite</li>
-                <li>Build something awesome</li>
+                {todos.map((todo, index) => (
+                    <li key={index}>{todo}</li>
+                ))}
             </ul>
         </div>
-    )
+    );
 }
